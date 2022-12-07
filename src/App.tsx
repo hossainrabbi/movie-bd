7335323c44911c166c24d15b5a424bcdb3cc8e2b
hossainrabbi <hossainrabbi259@gmail.com>
2022-12-07 14:53:45 +0600

    error page design end

--- src/App.tsx
+++ src/App.tsx
@@ -6,13 +6,14 @@ import {
 } from 'react-router-dom';
 import RootLayout from './layout/RootLayout';
 import About from './pages/About';
+import Error from './pages/Error';
 import Home from './pages/Home';
 import MovieDetails from './pages/MovieDetails';
 
 // create router elements with Routes
 const router = createBrowserRouter(
   createRoutesFromElements(
-    <Route path="/" element={<RootLayout />}>
+    <Route path="/" element={<RootLayout />} errorElement={<Error />}>
       <Route index element={<Home />} />
       <Route path="/movies/:movieId" element={<MovieDetails />} />
       <Route path="/about" element={<About />} />
---
+++ src/pages/Error.tsx
@@ -0,0 +1,30 @@
+import {
+  isRouteErrorResponse,
+  useNavigate,
+  useRouteError,
+} from 'react-router-dom';
+
+export default function Error() {
+  const navigate = useNavigate();
+  let error = useRouteError();
+  console.log(error);
+
+  return (
+    <div className="wrapper w-screen min-h-screen flex justify-center items-center text-center">
+      <div>
+        <h2 className="text-3xl font-medium text-gray-900">
+          Error is Occurred!
+        </h2>
+        {isRouteErrorResponse(error) && (
+          <p className="text-lg text-gray-700 mt-3">
+            {error.status} {error.statusText}
+          </p>
+        )}
+        <img className="max-h-96" src="/assets/images/error.jpg" alt="error" />
+        <button className="btn" onClick={() => navigate(-1)}>
+          Go Back
+        </button>
+      </div>
+    </div>
+  );
+}
